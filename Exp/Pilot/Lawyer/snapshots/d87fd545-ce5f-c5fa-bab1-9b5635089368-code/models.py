c8b1ee9e3e3112d3f8e87ea414b35694af192d29
Elisa Kreiss <elisakreiss@yahoo.de>
2018-03-20 10:26:50 +0100

    change country status and approve requirement

---
+++ Exp/Pilot/Lawyer/snapshots/d87fd545-ce5f-c5fa-bab1-9b5635089368-code/models.py
@@ -0,0 +1,32 @@
+from dallinger.nodes import Source
+import random
+
+
+class CrimeReportsSource(Source):
+    """A Source that reads in a random story from a file and transmits it."""
+
+    __mapper_args__ = {
+        "polymorphic_identity": "crime_reports_source"
+    }
+
+    def _contents(self):
+        """Define the contents of new Infos.
+
+        transmit() -> _what() -> create_information() -> _contents().
+
+        stories = [
+            "alexa.md",
+            "bees.md",
+            "flight.md",
+            "pigeon.md",
+            "sleep.md",
+            "smuggler.md"
+        ]
+        """
+        stories = [
+            "bees.md",
+            "flight.md"
+        ]
+        story = random.choice(stories)
+        with open("static/stimuli/{}".format(story), "r") as f:
+            return f.read()

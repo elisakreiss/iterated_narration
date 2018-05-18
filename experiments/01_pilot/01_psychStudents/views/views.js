5c70dd6c87550da06353bd2209a2fe8bdfc47e88
Michael Franke <the.secret.goldfish@me.com>
2018-05-18 10:14:47 +0200

    added notice to instructions

--- experiments/01_pilot/01_psychStudents/views/views.js
+++ experiments/01_pilot/01_psychStudents/views/views.js
@@ -31,7 +31,7 @@ var instructions = {
      // instruction's title
     "title": "Instructions",
     // instruction's text
-    "text": "In each of the three trials of this experiment, you will read a short story. You are then asked to retell this story. <strong>Please do not take notes.</strong> Make sure to retell the story immediately after reading the original. <p><strong>Notice:</strong>You may experience lags during the experiment, because the experiment communicates with a data base. Please just be patient, as the experiment should resume soon enough.",
+    "text": "In each of the three trials of this experiment, you will read a short story. You are then asked to retell this story. <strong>Please do not take notes.</strong> Make sure to retell the story immediately after reading the original. <p><strong>Notice:</strong> You may experience lags during the experiment, because the experiment communicates with a data base. Please just be patient, as the experiment should resume soon enough.",
     // instuction's slide proceeding button text
     "buttonText": "Let's go!",
     render: function() {

31ea0780ab75120d995f3dbb9925751994c217de
Elisa Kreiss <mail@elisakreiss.com>
2018-05-21 12:35:00 +0200

    fix generation_max and deadend; finalize for demo

--- experiments/01_pilot/01_psychStudents/config/config_deploy.js
+++ experiments/01_pilot/01_psychStudents/config/config_deploy.js
@@ -3,21 +3,21 @@ var config_deploy = {
     // obligatory fields
 	
     // needed to recover data from server app
-//    "author": "EK",
+   "author": "EK",
     // needed to recover data from server app
-//    "experiment_id": "IteratedNarration", 
-//    "description": "IteratedNarrationPilot",
+   "experiment_id": "IteratedNarrationDemo", 
+   "description": "IteratedNarrationDemoPilot",
 
     // set deployment method; use one of:
     //'debug', 'localServer', 'MTurk', 
     // 'MTurkSandbox', 'Prolific', 'directLink'
-    // "deployMethod" : "directLink", 
-    "deployMethod" : "debug", 
+    "deployMethod" : "directLink", 
+    // "deployMethod" : "debug", 
     
     // optional fields
     
     // who to contact in case of trouble
-    "contact_email": "YOUREMAIL@wherelifeisgreat.you", 
+    "contact_email": "mail@elisakreiss.com", 
 };
 
 

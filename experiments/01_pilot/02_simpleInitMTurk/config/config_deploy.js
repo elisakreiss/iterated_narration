571fbf812ee5805e4e19675bde5f7735e6ddef62
Elisa Kreiss <mail@elisakreiss.com>
2018-05-31 17:10:04 +0200

    first steps for mturk pilot

---
+++ experiments/01_pilot/02_simpleInitMTurk/config/config_deploy.js
@@ -0,0 +1,25 @@
+var config_deploy = {
+    
+    // obligatory fields
+	
+    // needed to recover data from server app
+   "author": "EK",
+    // needed to recover data from server app
+   "experiment_id": "IteratedNarrationDemo", 
+   "description": "IteratedNarrationDemoPilot",
+
+    // set deployment method; use one of:
+    //'debug', 'localServer', 'MTurk', 
+    // 'MTurkSandbox', 'Prolific', 'directLink'
+    // "deployMethod" : "directLink", 
+    "deployMethod" : "debug", 
+    
+    // optional fields
+    
+    // who to contact in case of trouble
+    "contact_email": "mail@elisakreiss.com", 
+};
+
+
+
+   
---
+++ experiments/01_pilot/02_simpleInitMTurk/scripts/experiment.js
@@ -0,0 +1,24 @@
+// customize the experiment by specifying a view order and a trial structure
+exp.customize = function() {
+
+	// record current date and time in global_data
+    this.global_data.startDate = Date();
+    this.global_data.startTime = Date.now();
+    this.global_data.browser = BrowserDetect.browser;
+    this.global_data.os = BrowserDetect.OS;
+	
+    // specify view order
+    this.views_seq = [intro, 
+					 instructions,
+					 main,
+                     postTest,
+                     thanks];
+	
+    // prepare information about trials (procedure)
+	// randomize main trial order, but keep practice trial order fixed
+    this.trial_info.main_trials = _.shuffle(main_trials)
+	
+};
+
+
+
